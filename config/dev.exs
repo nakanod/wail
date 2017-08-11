b9db01a0d04f323ded0d34b3adda4e5a123fce39
Daisuke Nakano <mail@nakanod.net>
2017-08-11 23:58:17 +0900

    database information refer environment variables.

--- config/dev.exs
+++ config/dev.exs
@@ -50,8 +50,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :wail, Wail.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "wail_dev",
-  hostname: "localhost",
+  username: System.get_env("DATABASE_USER"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  database: System.get_env("DATABASE_NAME"),
+  hostname: System.get_env("DATABASE_HOST"),
   pool_size: 10
